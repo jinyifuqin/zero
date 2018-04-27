ba34f3835f07da1838f995e3f104aa87fab0bc38
jinyifuqin <jinyifuqin@163.com>
2018-04-27 14:13:22 +0800

    d

---
+++ database/migrations/20180427061118_articles.php
@@ -0,0 +1,37 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class Articles extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('articles',array('engine'=>'InnoDB'));
+        $table->addColumn('content', 'string',array('default'=>'','comment'=>'文章内容'))
+            ->addColumn('title', 'string',array('limit' => 32,'default'=>'','comment'=>'文章标题'))
+            ->addColumn('create_time', 'datetime',array('default'=>'CURRENT_TIMESTAMP','comment'=>'时间'))
+            ->create();
+    }
+}
